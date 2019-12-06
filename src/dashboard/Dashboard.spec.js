9fa4e82ead52f16603d4343c1cca692babc74c83
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2019-12-05 20:53:12 -0600

    finished

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
 // Test away
+
+
+test("Render Parent Snapshot", async () => {
+    const wrapper = rtl.render(<Dashboard />);
+   expect(wrapper).toBeDefined();
+   expect(wrapper.asFragment()).toMatchSnapshot();
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,47 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
 // Test away!
+// test lockedClass & closedClass functions
+
+test("Display: Renders", async () => {
+  const wrapper = rtl.render(<Display />);
+  expect(wrapper).toBeDefined();
+});
+
+test("Display: test state", async () => {
+  const wrapper = rtl.render(<Display />);
+
+  const lockedClass = wrapper.findAllByDisplayValue(/lockedClass/i);
+  expect(lockedClass).toBeTruthy();
+  const closedClass = wrapper.findAllByDisplayValue(/closedClass/i);
+  expect(closedClass).toBeTruthy();
+});
+
+test("Display: closed & locked == false", async () => {
+  const wrapper = rtl.render(<Display />);
+
+  // in this case toBeTruthy == closed: false
+  const closed = wrapper.findAllByDisplayValue(/closed/i);
+  expect(closed).toBeTruthy();
+  const locked = wrapper.findAllByDisplayValue(/locked/i);
+  expect(locked).toBeTruthy();
+});
+
+test("test classes", async () => {
+  const wrapper = rtl.render(<Display />);
+
+  const closed = wrapper.findAllByDisplayValue(/closed/i);
+  const locked = wrapper.findAllByDisplayValue(/locked/i);
+
+  const lockedClass = wrapper.findAllByDisplayValue(/lockedClass/i);
+  const closedClass = wrapper.findAllByDisplayValue(/closedClass/i);
+
+//   expect(closed).toHaveClass("red-led");
+//   expect(locked).toHaveClass("red-led");
+
+// expect(closedClass).toHaveClass('.red-led');
+
+});
